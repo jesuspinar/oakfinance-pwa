b69b72b9c2bbac43a866f091c3e050958e7977c3
Sem Ekkelboom <sem.ekkelboom@gmail.com>
2025-11-27 12:08:26 +0100

    Fix: Add IonicModule.forRoot() to properly load Ionic components and styles

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
 import { Storage } from '@ionic/storage-angular';
+import { importProvidersFrom } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
@@ -11,6 +13,7 @@ import { provideServiceWorker } from '@angular/service-worker';
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    importProvidersFrom(IonicModule.forRoot({})),
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
     Storage, provideServiceWorker('ngsw-worker.js', {
